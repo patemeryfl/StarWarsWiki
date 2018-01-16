4b00cdf24d9aed74d8dbe5c77acf5c49502cdf5d
Patrick Emery <patrickemery@mail.usf.edu>
2018-01-16 16:45:03 -0500

    Initialize Repo, Add Layout/NPM Packages, Query Structures

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from "gatsby-link";
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="content">
+            <div className="inner">
+                <h1>StarWars Wikipeda</h1>
+                <p>An interactive wiki for StarWars fans </p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Films</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Planets</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Characters</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Vehicles</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: React.PropTypes.func,
+    timeout: React.PropTypes.bool
+}
+
+export default Header
---
+++ src/layouts/index.js
@@ -0,0 +1,134 @@
+import React from 'react'
+import '../assets/scss/main.scss'
+import Helmet from 'react-helmet'
+
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
+
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://localhost:3000',
+  }),
+  connectToDevTools: true,
+});
+
+class Template extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isArticleVisible: false,
+      timeout: false,
+      articleTimeout: false,
+      article: '',
+      loading: 'is-loading'
+    }
+    this.handleOpenArticle = this.handleOpenArticle.bind(this)
+    this.handleCloseArticle = this.handleCloseArticle.bind(this)
+  }
+
+  componentDidMount () {
+    this.timeoutId = setTimeout(() => {
+        this.setState({loading: ''});
+    }, 100);
+  }
+
+  componentWillUnmount () {
+    if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+    }
+  }
+
+  handleOpenArticle(article) {
+
+    this.setState({
+      isArticleVisible: !this.state.isArticleVisible,
+      article
+    })
+
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout
+      })
+    }, 325)
+
+    setTimeout(() => {
+      this.setState({
+        articleTimeout: !this.state.articleTimeout
+      })
+    }, 350)
+
+  }
+
+  handleCloseArticle() {
+
+    this.setState({
+      articleTimeout: !this.state.articleTimeout
+    })
+
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout
+      })
+    }, 325)
+
+    setTimeout(() => {
+      this.setState({
+        isArticleVisible: !this.state.isArticleVisible,
+        article: ''
+      })
+    }, 350)
+
+  }
+
+  render() {
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const siteDescription = this.props.data.site.siteMetadata.description
+
+    return (
+      <ApolloProvider client={client}>
+        <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
+          <Helmet>
+              <title>{siteTitle}</title>
+              <meta name="description" content={siteDescription} />
+          </Helmet>
+
+          <div id="wrapper">
+
+            <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
+            <Main
+              isArticleVisible={this.state.isArticleVisible}
+              timeout={this.state.timeout}
+              articleTimeout={this.state.articleTimeout}
+              article={this.state.article}
+              onCloseArticle={this.handleCloseArticle}
+            />
+            <Footer timeout={this.state.timeout} />
+
+          </div>
+          <div id="bg"></div>
+        </div>
+      </ApolloProvider>
+    )
+  }
+}
+
+Template.propTypes = {
+  route: React.PropTypes.object,
+}
+
+export default Template
+
+export const pageQuery = graphql`
+  query PageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
